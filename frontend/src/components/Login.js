d4994f2fd54c82255761ae6bad8061e9898da273
TylerM131 <60491900+TylerM131@users.noreply.github.com>
2020-10-24 10:48:05 -0400

    Add files via upload

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 function Login() {
   var loginName;
@@ -6,22 +7,32 @@ function Login() {
 
   const [message, setMessage] = useState('');
 
-  const doLogin = async event => {
+  /*const doLogin = async event => {
     event.preventDefault();
 
     alert('doIt() ' + loginName.value + ' ' + loginPassword.value);
-  };
+  };*/
 
   return (
     <div id="loginDiv">
-      <p>THIS IS A TEST OF HEROKU</p>
-      <form onSubmit={doLogin}>
-        <span id="inner-title">PLEASE LOG IN</span><br />
-        <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} />
-        <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} />
-        <input type="submit" id="loginButton" class="buttons" value="Do It" onClick={doLogin} />
-      </form>
-      <span id="loginResult">{message}</span>     </div>
+      <div class="title">
+        <span class="titleA">A</span><span class="titleThena">thena</span>
+      </div>
+      
+      <div style="background: url(./gold.jpg);">
+        <form onSubmit={doLogin}>
+          
+          <input type="text" id="loginName" placeholder="Email or Username" ref={(c) => loginName = c} />
+          <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} />
+          <span toggle="#password" id = "pwd" class="fa fa-fw fa-eye field-icon pwd"></span>
+          <input type="checkbox" id="remember"> Remember Me</input>
+          <input type="submit" id="loginButton" class="buttons" value="Do It" onClick={doLogin} />
+          <p id="response"></p> {/*error messages for login go here*/}
+        </form>
+
+        <a class="signup-login-link" href="Signup.js"></p>    
+      </div>
+    </div>
   );
 };
 
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup()
+{
+    return
+    (
+        <div>
+            <p>This will be the Sign Up Page</p>
+        </div>
+    );
+};
+
+export default Signup;
